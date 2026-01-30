10cd15efe633bb68f1c8fe97dfa9d8a712d498a3
Nodir-bek581 <nodirbekodilov581@gmail.com>
2026-01-30 10:41:21 +0500

    problems

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main() {
-        System.out.print("Hello World!\n");
-        System.out.print("Hello world");
+        System.out.print("Hello, what is your name: ");
+        Scanner input = new Scanner(System.in);
+        String name=input.nextLine();
+        System.out.print("How are you, "+name+", how old are you ");
+        String age = input.nextLine();
+        int age1 = Integer.parseInt(age);
+        System.out.println(name+", you are "+age);
+
     }
 }
